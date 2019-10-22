6ca3a5061f075918df9ebdb8be30383f690053d9
Andr3w0w3n <waowen17@gmail.com>
2019-10-22 14:38:30 -0600

    search updated

--- binarytree.c
+++ binarytree.c
@@ -3,23 +3,36 @@
 #include <stdlib.h>
 #include "binarytree.h"
 
+//Tim's
 void initialize(binary_tree* bt){
 
 }
 
 //Andrew's
 bool search(binary_tree* bt, int key){
-
+    if(bt->/*int value*/ == NULL){
+        return false;
+    }
+    else if(key < bt->/*int value*/){
+        return search(/*left reference*/, key);
+    }else if(key > bt->/*int value*/){
+        return search(/*right reference*/, key);
+    }else{
+        return true;
+    }
 }
 
+//Meaghan's
 void insert(binary_tree* bt, int item){
 
 }
 
+//Tim's
 void printinorder(binary_tree* bt){
 
 }
 
+//Meaghan's
 void printpreorder(binary_tree* bt){
 
 }
@@ -29,6 +42,7 @@ void printpostorder(binary_tree* bt){
 
 }
 
+//Meaghan's
 int btsize(binary_tree* bt){
 
 }
