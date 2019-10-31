f6447b24e44a94768901657858cd6632ac981faa
Andr3w0w3n <waowen17@gmail.com>
2019-10-30 20:01:42 -0600

    first update to main.c

--- main.c
+++ main.c
@@ -1,10 +1,32 @@
 #include "binarytree.h"
+#include "node.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 int main(void){
+    //variables
+    binary_tree bt;
+    bool found;
 
+    //copied from assignment PDF
+    printf("Print in order\n");
+    printinorder(&bt);
+    printf("\nPrint pre order\n");
+    printpreorder(&bt);
+    printf("\nPrint post order\n");
+    printpostorder(&bt);
+    printf("\n");
+    found = search(&bt,33);
+    if (found){
+        printf("FOUND 33\n");
+    }
+    found = search(&bt,38);
+    if (found){
+        printf("FOUND 38\n");
+    }
+    printf("Number of elements in tree: %d\n", btsize(&bt));
+    printf("Tree height: %d\n", treeheight(&bt));
 
     return 0;
 }
